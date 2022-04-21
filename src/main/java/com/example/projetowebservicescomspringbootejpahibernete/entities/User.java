6809489707382161788a6796ad56d7c316901de9
williamsartijose <williamsartijose@hotmail.com>
2022-04-20 23:57:38 -0300

    H2 database, test profile, JPA

--- src/main/java/com/example/projetowebservicescomspringbootejpahibernete/entities/User.java
+++ src/main/java/com/example/projetowebservicescomspringbootejpahibernete/entities/User.java
@@ -2,11 +2,19 @@ package com.example.projetowebservicescomspringbootejpahibernete.entities;
 
 import org.springframework.core.annotation.Order;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
