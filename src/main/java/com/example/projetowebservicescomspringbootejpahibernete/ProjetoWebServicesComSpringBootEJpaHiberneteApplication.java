b6530b1c0cee618038a753c2a20315bd45dddc2d
williamsartijose <williamsartijose@hotmail.com>
2022-04-20 22:25:34 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/projetowebservicescomspringbootejpahibernete/ProjetoWebServicesComSpringBootEJpaHiberneteApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetowebservicescomspringbootejpahibernete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebServicesComSpringBootEJpaHiberneteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoWebServicesComSpringBootEJpaHiberneteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projetowebservicescomspringbootejpahibernete/ProjetoWebServicesComSpringBootEJpaHiberneteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetowebservicescomspringbootejpahibernete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServicesComSpringBootEJpaHiberneteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
