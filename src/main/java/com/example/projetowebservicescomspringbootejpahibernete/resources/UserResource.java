fda32f094e55097b7012687b688c756dd5bed1ad
williamsartijose <williamsartijose@hotmail.com>
2022-04-21 11:46:48 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/example/projetowebservicescomspringbootejpahibernete/resources/UserResource.java
+++ src/main/java/com/example/projetowebservicescomspringbootejpahibernete/resources/UserResource.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "William", "williamsarti@gmail.com", "18997874643", "12345");
-        return  ResponseEntity.ok().body(u);
-
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "William", "maria@gmail.com", "9999999", "12345");
+        return ResponseEntity.ok().body(u);
     }
 }
